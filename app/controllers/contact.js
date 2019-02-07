f036445b0b0bab34a6587b41c10461f2af93f84c
Nilima <lnilimayadav97@gmail.com>
2019-02-07 17:06:52 +0530

    update

---
+++ app/controllers/contact.js
@@ -0,0 +1,30 @@
+
+
+
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+ isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+ isMessageEnoughLong: Ember.computed.gte('message.length', 5),
+ emailAddress: '',
+ text: '',
+ isValid: Ember.computed.and('isValidEmail', 'isMessageEnoughLong'),
+
+ actions: {
+
+   saveInvite() {
+     const email = this.get('emailAddress');
+     const text = this.get('text');
+     const newContact = this.store.createRecord('Contact', { email ,text});
+
+     newContact.save().then(response => {
+       this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+       this.set('emailAddress', '',);
+       this.set('text','');
+     });
+
+   }
+ }
+
+});
---
+++ app/routes/libraries/new.js
@@ -0,0 +1,21 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+	model()
+	{
+		return this.store.createRecord('library');
+	},
+	actions:{
+		saveLibrary(newLibrary){
+			newLibrary.save().then(() => this.transitionTo('libraries'));
+
+		},
+		willTransition() {
+  let model = this.controller.get('model');
+
+  if (model.get('isNew')) {
+    model.destroyRecord();
+  }
+}
+	}
+});
